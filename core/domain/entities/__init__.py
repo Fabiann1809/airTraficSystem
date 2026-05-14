6f07c20edbce8f9a4476d4f7f1ad763893e9807b
F3L1P3GoD <felipe.gamboa0102@gmail.com>
2026-05-14 17:57:15 -0500

    feat: exporta entidades del dominio desde el paquete entities
    Agrega __all__ con Aircraft, Incident y Quadrant para permitir
    importaciones limpias desde cualquier capa sin depender de
    la ruta interna de cada modulo.

---
+++ core/domain/entities/__init__.py
@@ -0,0 +1,5 @@
+from core.domain.entities.aircraft import Aircraft
+from core.domain.entities.incident import Incident
+from core.domain.entities.quadrant import Quadrant
+
+__all__ = ["Aircraft", "Incident", "Quadrant"]
